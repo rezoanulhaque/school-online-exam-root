fd7e28e85f573d149245ec615c50efe9e3523240
Rezoanul Haque <rezoanhaque2012@gmail.com>
2021-06-11 23:57:33 +0600

    add commit 4

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.connect(err => {
   const examQuestionCollection = client.db(`${process.env.DB_NAME}`).collection("examQuestion");
   const teacherCollection = client.db(`${process.env.DB_NAME}`).collection("teacher");
   const studentCollection = client.db(`${process.env.DB_NAME}`).collection("student");
- 
+ /*user */
   app.get('/teacher', (req, res) => {
     const userEmail = req.query.email;
     teacherCollection.find({ email: userEmail })
@@ -53,6 +53,8 @@ app.patch('/updatedInfo', (req, res) =>{
     res.send(result.modifiedCount > 0)
   })
 })
+/*user */
+/*add Question */
 app.post('/addNewQuestionToHsc', (req, res) => {
   const addQuestion = req.body
   classelevenqbCollection.insertOne(addQuestion)
@@ -103,6 +105,8 @@ app.post('/addNewQuestionToOne', (req, res) => {
   classoneqbCollection.insertOne(addQuestion)
       .then(result => res.send(result.insertedCount > 0 ))
 })
+/*add Question */
+/*get Question */
 app.get('/getQuestionFromHsc', (req, res) => {
   const subjectName = req.query.subject
   classelevenqbCollection.find({subject : subjectName})
@@ -110,9 +114,135 @@ app.get('/getQuestionFromHsc', (req, res) => {
     res.send(documents)
   })
 })
+app.get('/getQuestionFromSsc', (req, res) => {
+  const subjectName = req.query.subject
+  classnineqbCollection.find({subject : subjectName})
+  .toArray((err, documents) => {
+    res.send(documents)
+  })
+})
+app.get('/getQuestionFromJsc', (req, res) => {
+  const subjectName = req.query.subject
+  classeightqbCollection.find({subject : subjectName})
+  .toArray((err, documents) => {
+    res.send(documents)
+  })
+})
+app.get('/getQuestionFromSeven', (req, res) => {
+  const subjectName = req.query.subject
+  classsevenqbCollection.find({subject : subjectName})
+  .toArray((err, documents) => {
+    res.send(documents)
+  })
+})
+app.get('/getQuestionFromSix', (req, res) => {
+  const subjectName = req.query.subject
+  classsixqbCollection.find({subject : subjectName})
+  .toArray((err, documents) => {
+    res.send(documents)
+  })
+})
+app.get('/getQuestionFromFive', (req, res) => {
+  const subjectName = req.query.subject
+  classfiveqbCollection.find({subject : subjectName})
+  .toArray((err, documents) => {
+    res.send(documents)
+  })
+})
+app.get('/getQuestionFromFour', (req, res) => {
+  const subjectName = req.query.subject
+  classfourqbCollection.find({subject : subjectName})
+  .toArray((err, documents) => {
+    res.send(documents)
+  })
+})
+app.get('/getQuestionFromThree', (req, res) => {
+  const subjectName = req.query.subject
+  classthreeqbCollection.find({subject : subjectName})
+  .toArray((err, documents) => {
+    res.send(documents)
+  })
+})
+app.get('/getQuestionFromTwo', (req, res) => {
+  const subjectName = req.query.subject
+  classtwoqbCollection.find({subject : subjectName})
+  .toArray((err, documents) => {
+    res.send(documents)
+  })
+})
+app.get('/getQuestionFromOne', (req, res) => {
+  const subjectName = req.query.subject
+  classoneqbCollection.find({subject : subjectName})
+  .toArray((err, documents) => {
+    res.send(documents)
+  })
+})
+/*get Question */
+/*delete single Question */
 app.delete('/deleteSingleQuestionFromHsc/:id', (req, res) =>{
   const id=req.params.id
   classelevenqbCollection.deleteOne({_id: ObjectId(id)})
+  .then(result => 
+    res.send(result.deletedCount > 0))
+})
+app.delete('/deleteSingleQuestionFromSsc/:id', (req, res) =>{
+  const id=req.params.id
+  classnineqbCollection.deleteOne({_id: ObjectId(id)})
+  .then(result => 
+    res.send(result.deletedCount > 0))
+})
+app.delete('/deleteSingleQuestionFromJsc/:id', (req, res) =>{
+  const id=req.params.id
+  classeightqbCollection.deleteOne({_id: ObjectId(id)})
+  .then(result => 
+    res.send(result.deletedCount > 0))
+})
+app.delete('/deleteSingleQuestionFromSeven/:id', (req, res) =>{
+  const id=req.params.id
+  classsevenqbCollection.deleteOne({_id: ObjectId(id)})
+  .then(result => 
+    res.send(result.deletedCount > 0))
+})
+app.delete('/deleteSingleQuestionFromSix/:id', (req, res) =>{
+  const id=req.params.id
+  classsixqbCollection.deleteOne({_id: ObjectId(id)})
+  .then(result => 
+    res.send(result.deletedCount > 0))
+})
+app.delete('/deleteSingleQuestionFromFive/:id', (req, res) =>{
+  const id=req.params.id
+  classfiveqbCollection.deleteOne({_id: ObjectId(id)})
+  .then(result => 
+    res.send(result.deletedCount > 0))
+})
+app.delete('/deleteSingleQuestionFromFour/:id', (req, res) =>{
+  const id=req.params.id
+  classfourqbCollection.deleteOne({_id: ObjectId(id)})
+  .then(result => 
+    res.send(result.deletedCount > 0))
+})
+app.delete('/deleteSingleQuestionFromThree/:id', (req, res) =>{
+  const id=req.params.id
+  classthreeqbCollection.deleteOne({_id: ObjectId(id)})
+  .then(result => 
+    res.send(result.deletedCount > 0))
+})
+app.delete('/deleteSingleQuestionFromTwo/:id', (req, res) =>{
+  const id=req.params.id
+  classtwoqbCollection.deleteOne({_id: ObjectId(id)})
+  .then(result => 
+    res.send(result.deletedCount > 0))
+})
+app.delete('/deleteSingleQuestionFromOne/:id', (req, res) =>{
+  const id=req.params.id
+  classoneqbCollection.deleteOne({_id: ObjectId(id)})
+  .then(result => 
+    res.send(result.deletedCount > 0))
+})
+/*delete single Question */
+app.delete('/deleteResultOfExam/:id', (req, res) =>{
+  const id=req.params.id
+  examQuestionCollection.deleteOne({_id: ObjectId(id)})
   .then(result => 
     res.send(result.deletedCount > 0))
 })
